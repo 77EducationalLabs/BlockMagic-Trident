80330ba2806b69f81d19029d9d8e497ab885f813
Gabriel Thome <eugabrielthome@gmail.com>
2024-06-02 19:00:50 -0300

    fix(frontend-dapp): games rating

--- frontend-dapp/src/components/Game.js
+++ frontend-dapp/src/components/Game.js
@@ -21,7 +21,7 @@ function Game({ title, price, image, category, subcategory, rating, description,
         <div className='game-rating-wrap'>
           <p className="game-rating">
             <span>
-            {rating}/10
+            {rating}/5
             </span>
             Rating
           </p> 
